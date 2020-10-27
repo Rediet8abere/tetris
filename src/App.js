552d451ffb03b842f6f2a6d4244ccbdd28b1b604
Rediet8abere <redietaberegesesse@gmail.com>
2020-10-26 17:38:34 -0700

    Added initial score board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import GridBoard from './components/GridBoard'
 import './App.css';
+
+import GridBoard from './components/GridBoard'
+import NextBlock from './components/NextBlock'
+import ScoreBoard from './components/ScoreBoard'
 
 function App() {
   return (
@@ -8,9 +11,10 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
-      <GridBoard color="4" />
+      <GridBoard />
+      <NextBlock />
+      <ScoreBoard />
     </div>
   );
 }
-
 export default App;
