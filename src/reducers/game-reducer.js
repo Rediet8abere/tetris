5217a3671b07f3ee140a19fe9b2fc8400e80d628
Rediet8abere <redietaberegesesse@gmail.com>
2020-11-03 08:13:07 -0800

    restart implemented

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -71,7 +71,7 @@ const gameReducer = (state = defaultState(), action) => {
     case GAME_OVER:
       return state
     case RESTART:
-      return state
+      return defaultState()
     default:
       return state
   }
